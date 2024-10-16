9f92bacd71e826229e30c30cc3dc4c67a8542203
RubensCarvalhoRocha <rocharubens4@gmail.com>
2024-10-16 20:19:15 -0300

    --

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CardModule, InputTextModule, ButtonModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
+  loginFailed: boolean = false;
 
+  login() {
+    // Lógica de login aqui
+    console.log('Login clicked');
+  }
 }
