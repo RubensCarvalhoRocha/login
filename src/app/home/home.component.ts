ef5588d60de4211e499dc016589488e360934b12
RubensCarvalhoRocha <rocharubens4@gmail.com>
2024-10-16 20:37:20 -0300

    .

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +10,24 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  constructor(private router: Router, private authService: AuthService) {}
+
+  navigateTo(route: string) {
+    this.router.navigate([`/${route}`]);
+  }
+
+  isAdmin(): boolean {
+    return this.authService.getRole() === 'admin';
+  }
+
+  isGerenteOrAdmin(): boolean {
+    const role = this.authService.getRole();
+    return role === 'admin' || role === 'gerente';
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 
 }
